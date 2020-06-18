d5fa1aff029afde344e9c6a2f0240ac7410d1dea
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-06-18 13:29:50 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# admin-product-category-image
+
+## Instalasi
+
+Jalankan perintah di bawah di folder aplikasi:
+
+```
+mim app install admin-product-category-image
+```
---
+++ modules/admin-product-category-image/config.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    '__name' => 'admin-product-category-image',
+    '__version' => '0.0.1',
+    '__git' => 'git@github.com:getmim/admin-product-category-image.git',
+    '__license' => 'MIT',
+    '__author' => [
+        'name' => 'Iqbal Fauzi',
+        'email' => 'iqbalfawz@gmail.com',
+        'website' => 'http://iqbalfn.com/'
+    ],
+    '__files' => [
+        'modules/admin-product-category-image' => ['install','update','remove']
+    ],
+    '__dependencies' => [
+        'required' => [
+            [
+                'admin-product-category' => NULL
+            ],
+            [
+                'lib-upload' => NULL 
+            ]
+        ],
+        'optional' => []
+    ],
+    'autoload' => [
+        'classes' => [],
+        'files' => []
+    ],
+    'libForm' => [
+        'forms' => [
+            'admin.product-category.edit' => [
+                'image' => [
+                    'label' => 'Image',
+                    'type' => 'image',
+                    'form' => 'std-image',
+                    'rules' => []
+                ]
+            ]
+        ]
+    ]
+];
